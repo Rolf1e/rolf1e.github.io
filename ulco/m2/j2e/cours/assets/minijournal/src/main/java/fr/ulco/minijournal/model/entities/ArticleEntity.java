e6fac70a458f416dfe4beb58c276d5cd8fa982c6
Rolf1e <tigran.slama@contentsquare.com>
2023-02-02 14:37:24 +0100

    Update wspring project with IDENTITY for PK

--- ulco/m2/j2e/cours/assets/minijournal/src/main/java/fr/ulco/minijournal/model/entities/ArticleEntity.java
+++ ulco/m2/j2e/cours/assets/minijournal/src/main/java/fr/ulco/minijournal/model/entities/ArticleEntity.java
@@ -16,7 +16,7 @@ import java.util.Collection;
 public class ArticleEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "title")
--- ulco/m2/j2e/cours/assets/minijournal/src/main/java/fr/ulco/minijournal/model/entities/AuthorEntity.java
+++ ulco/m2/j2e/cours/assets/minijournal/src/main/java/fr/ulco/minijournal/model/entities/AuthorEntity.java
@@ -15,7 +15,7 @@ import java.util.Collection;
 public class AuthorEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
